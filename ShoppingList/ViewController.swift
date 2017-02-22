e8c77f71155415e0c5abc51c07685fd1f76d73de
rushabh <rushabh@rushabh.local>
2017-02-21 19:48:39 -0500

    Initial Commit

---
+++ ShoppingList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by rushabh on 21/02/17.
+//  Copyright © 2017 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
